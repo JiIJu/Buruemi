c4c046cf1ee9df60939989a873681a60ae429901
JiIJu <eldk313131@gmail.com>
2023-04-13 17:22:43 +0900

    feature/ROS_study_ij

---
+++ ROS/robot_ws/build/tf2_rclpy_example/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/ssafy/robot_ws/install/tf2_rclpy_example'
---
+++ ROS/robot_ws/install/testbot_description/share/testbot_description/local_setup.sh
@@ -0,0 +1,1 @@
+/home/ssafy/robot_ws/build/testbot_description/ament_cmake_environment_hooks/local_setup.sh
