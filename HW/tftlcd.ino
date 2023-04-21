64c3cd5bf4cbce58096040264fd74d59cc554b6c
JiIJu <eldk313131@gmail.com>
2023-04-21 17:43:19 +0900

    Update HW/tftlcd.ino

--- HW/tftlcd.ino
+++ HW/tftlcd.ino
@@ -1,25 +1,12 @@
-
-// Adafruit_ImageReader test for Adafruit ILI9341 TFT Shield for Arduino.
-// Demonstrates loading images from SD card or flash memory to the screen,
-// to RAM, and how to query image file dimensions.
-// Requires three BMP files in root directory of SD card:
-// purple.bmp, parrot.bmp and wales.bmp.
-// As written, this uses the microcontroller's SPI interface for the screen
-// (not 'bitbang') and must be wired to specific pins (e.g. for Arduino Uno,
-// MOSI = pin 11, MISO = 12, SCK = 13). Other pins are configurable below.
-
 #include <Adafruit_GFX.h>         // Core graphics library
 #include <Adafruit_ILI9341.h>     // Hardware-specific library
 #include <SdFat.h>                // SD card & FAT filesystem library
 #include <Adafruit_SPIFlash.h>    // SPI / QSPI flash library
 #include <Adafruit_ImageReader.h> // Image-reading functions
 #include <string.h>
-// Comment out the next line to load from SPI/QSPI flash instead of SD card:
+
+
 #define USE_SD_CARD
-
-// TFT display and SD card share the hardware SPI interface, using
-// 'select' pins for each to identify the active device on the bus.
-
 #define SD_CS   4 // SD card select pin
 #define TFT_CS 10 // TFT select pin
 #define TFT_DC  9 // TFT display/command pin
@@ -60,12 +47,8 @@ void setup(void) {
 
   tft.begin();          // Initialize screen
 
-  // The Adafruit_ImageReader constructor call (above, before setup())
-  // accepts an uninitialized SdFat or FatVolume object. This MUST
-  // BE INITIALIZED before using any of the image reader functions!
   Serial.print(F("Initializing filesystem..."));
 #if defined(USE_SD_CARD)
-  // SD card is pretty straightforward, a single call...
   if(!SD.begin(SD_CS, SD_SCK_MHZ(25))) { // ESP32 requires 25 MHz limit
     Serial.println(F("SD begin() failed"));
     for(;;); // Fatal error, do not continue
@@ -84,24 +67,8 @@ void setup(void) {
 #endif
   Serial.println(F("OK!"));
 
-  // Fill screen blue. Not a required step, this just shows that we're
-  // successfully communicating with the screen.
   tft.fillScreen(ILI9341_BLUE);
 
-  // Load full-screen BMP file 'purple.bmp' at position (0,0) (top left).
-  // Notice the 'reader' object performs this, with 'tft' as an argument.
-  // Serial.print(F("Loading purple.bmp to screen..."));
-  // stat = reader.drawBMP("/purple.bmp", tft, 0, 0);
-  // reader.printStatus(stat);   // How'd we do?
-
-  // if(stat == IMAGE_SUCCESS) { // If it worked, print image size...
-  //   Serial.print(F("Image dimensions: "));
-  //   Serial.print(width);
-  //   Serial.write('x');
-  //   Serial.println(height);
-  // }
-
-  // delay(2000); // Pause 2 seconds before moving on to loop()
 }
 
 void loop() {
